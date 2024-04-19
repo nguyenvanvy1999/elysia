fc2a0e0137fb76879ff7c548bd201337815af942
Nguyen Van Vy <nguyenvanvy1999@gmail.com>
2024-04-19 11:38:52 +0700

    feat: init project

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,19 @@
+import { logger } from "@bogeychan/elysia-logger";
+import { cors } from "@elysiajs/cors";
+import { serverTiming } from "@elysiajs/server-timing";
+import { staticPlugin } from "@elysiajs/static";
+import { swagger } from "@elysiajs/swagger";
 import { Elysia } from "elysia";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app = new Elysia()
+	.use(serverTiming())
+	.use(cors())
+	.use(swagger())
+	.use(staticPlugin())
+	.use(logger({ level: "error" }))
+	.get("/", () => "Hello Elysia")
+	.listen(3000);
 
 console.log(
-  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
+	`🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`,
 );
