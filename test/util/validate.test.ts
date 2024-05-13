995051d6ebea2fdf6345196130f64bf6dbfdd244
Nguyen Van Vy <nguyenvanvy1999@gmail.com>
2024-05-13 17:28:53 +0700

    test: add test for validate function

--- test/util/validate.test.ts
+++ test/util/validate.test.ts
@@ -1,17 +1,29 @@
 import { describe, expect, it } from "bun:test";
-import { checkNumber } from "src/util";
+import { checkJSONString, checkNumber } from "src/util";
 
 describe("Util: Validate", () => {
-	const mockNumber = "1234";
-
 	describe("checkNumber", () => {
 		it("should return true when given a valid number", () => {
-			expect(checkNumber(mockNumber)).toBe(true);
+			expect(checkNumber("1234")).toBe(true);
 		});
 
 		it("should return false when given an invalid number", () => {
 			expect(checkNumber("not a number")).toBe(false);
 		});
+
+		it("should return true when given float number", () => {
+			expect(checkNumber("123.2")).toBe(true);
+			expect(checkNumber("123,2")).toBe(true);
+		});
 	});
-	//todo: add test for check json
+
+	describe("checkJSONString", () => {
+		it("should return true when given a valid JSON", () => {
+			expect(checkJSONString(JSON.stringify({ test: 1 }))).toBe(true);
+		});
+
+		it("should return false when given valid JSON", () => {
+			expect(checkJSONString("not a number")).toBe(false);
+		});
+	});
 });
